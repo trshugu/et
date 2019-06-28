860cc6aa3f86f303af5f8403dafa94d35aa2e478
aaa <aa@a>
2019-06-28 15:50:52 +0900

    gitpush2019/06/28

--- tmp.exs
+++ tmp.exs
@@ -1,9 +1,30 @@
 
 
 
-
-
+# https://devhints.io/elixir
 # for n <- [1,2,3,4,5], rem(n,2) == 1, do: n*n
+
+
+
+
+
+
+
+
+"""
+
+# 再代入防止 →なんかちがう・・・
+IO.puts "あああ"
+
+a = "いいい"
+IO.puts to_string a
+a = "eee"
+IO.puts a
+
+b = "うううう"
+IO.puts b
+
+
 
 
 defmodule Fun do
@@ -15,8 +36,26 @@ defmodule Fun do
   end
 end
 
-# IO.puts Fun 5
+# IO.puts Fun.fib(10)
 
+
+
+defmodule Greeter do
+  def greet(name) do
+    message = "Hello, " <> name <> "!"
+    IO.puts message
+  end
+end
+
+
+# Greeter.greet("world")
+
+
+
+1..10
+|> Enum.map(fn(x)-> x * x end)
+|> Enum.filter(fn(x)->x<40 end)
+|> IO.inspect
 
 
 
@@ -25,5 +64,7 @@ end
 # IO.puts a
 
 
-
 # IO.puts "Hell World"
+"""
+
+
